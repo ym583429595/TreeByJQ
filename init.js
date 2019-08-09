29429abafa0d4a03b66ab36e4bc40fd0171a2dd1
ym583429595 <ym583429595>
2019-08-09 16:16:47 +0800

    first

---
+++ init.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($){jQuery.easing.def="easeInOutBack";var $span=$('<span class="colourful"></span>');$span.insertBefore($("#menu ul"));var $menu_link=$('#menu li a'),$hovered=$('#menu a.hovered'),$hovered_pos=$hovered.position('#menu');var $colour_arr=['#fbb92e','#f8d52f','#b4f62f','#54f7a8','#3ff7f3','#3a97fa','#6835f9','#d544f6','#f650ab'];$menu_link.each(function(index){$menu_link.eq(index).css('border-color',$colour_arr[index]);});function init(){if($hovered_pos){$span.css('left',$hovered_pos);var index=0;for(i=0;i<$menu_link.length;i++){if($($menu_link[i]).hasClass('hovered')){index=i;}}
+$span.css('background',$colour_arr[index]);}
+$menu_link.each(function(intIndex){$(this).on("mouseenter",function(event){var x=$(this).position('#menu');x=x.left;$span.css('background',$colour_arr[intIndex]);$span.stop();$span.animate({left:x},600);});});$menu_link.each(function(intIndex){$(this).on("mouseleave",function(event){$span.stop();var x=-100;if($hovered_pos){x=$hovered_pos;var index=0;for(i=0;i<$menu_link.length;i++){if($($menu_link[i]).hasClass('hovered')){index=i;}}
+$span.css('background',colour_arr[index]);}
+$span.animate({left:x},600);});});}
+init();});
